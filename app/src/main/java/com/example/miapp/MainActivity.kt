72e249e69816c687ee7ca198c093ebd4cd4ed7bb
JersonSENA <jerson_solis@soy.sena.edu.co>
2024-10-23 19:59:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/miapp/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.miapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun MainScreen() {
+    var message by remember { mutableStateOf("¡Hola, Mundo!") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(text = message, fontSize = 24.sp)
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { message = "¡Botón Presionado!" }) {
+            Text("Presionar")
+        }
+    }
+}
